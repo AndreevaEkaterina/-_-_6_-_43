5267e402ebe165ef4e074017d96dc4ece8c70408
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-22 23:11:33 +0800

    решила задачу 43

--- Program.cs
+++ Program.cs
@@ -4,3 +4,26 @@
 // пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 =
 // 9 -> (-0,5; -0,5)
+void Coordinates(int A1, int B1, int A2, int B2)
+{
+    if (B1 == B2)
+    {
+        Console.WriteLine("Прямые параллельны.");
+    }
+    else
+    {
+        int x = (A2 - A1) / (B1 - B2);
+        int y = A1 * x + B1;
+        Console.WriteLine($"Пересечение в точке: ({x};{y})");
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+Coordinates(b1, k1, b2, k2);
